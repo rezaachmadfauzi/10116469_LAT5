92c303cd36d68eb7000f94c5b87fff6bae5fddd4
Reza Achmad Fauzi <rezaafofficial@gmail.com>
2017-11-14 22:19:18 +0700

    upload

---
+++ src/if11_10116469_latihanuts5/BangunDatar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if11_10116469_latihanuts5;
+
+/**
+ *
+ * @author Reza Achmad Fauzi
+ */
+ public interface BangunDatar {
+ public void  hitungLuas();
+ public void  hitungKeliling();
+}
+
+
---
+++ src/if11_10116469_latihanuts5/IF11_10116469_latihanUTS5.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if11_10116469_latihanuts5;
+
+/**
+ *
+ * @author Reza Achmad Fauzi
+ */
+public class IF11_10116469_latihanUTS5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JajarGenjang GJ = new JajarGenjang();
+        GJ.hitungLuas();
+        GJ.hitungKeliling();
+        System.out.println("\n");
+        
+        Segitiga SG = new Segitiga();
+        SG.hitungLuas();
+        SG.hitungKeliling();
+    }
+    
+}
